c74445d305da8a7dacef55d8ba1c50237b80492d
David Kitley-McNamara <david@kitley-mcnamara.com>
2021-08-24 21:49:34 +0100

    feat(JS): add const from button elements

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,3 +3,10 @@ const homeContainer = document.querySelector('#home-container');
 const quizContainer = document.querySelector('#quiz-container');
 const userFinalScoreContainer = document.querySelector('#user-final-score-container');
 const highScoresContainer = document.querySelector('#high-score-container');
+
+//* grab the button elements for the event listeners
+const playButton = document.querySelector('#btn-play-game');
+const homeScreenButton = document.querySelector('#btn-view-home-screen');
+const returnHomeScreenButton = document.querySelector('#btn-return-to-home-screen');
+const viewHighScoresButton = document.querySelector('#btn-view-high-scores');
+const saveScoreBtn = document.getElementById('btn-save-score');
