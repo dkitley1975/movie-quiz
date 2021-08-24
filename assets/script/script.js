694923c2dd3b9801432b055c3dc8e9dc50d1fb7f
David Kitley-McNamara <david@kitley-mcnamara.com>
2021-08-24 21:47:00 +0100

    feat(JS): add const from containers

---
+++ assets/script/script.js
@@ -0,0 +1,5 @@
+//* grab sites different containers for show/hiding
+const homeContainer = document.querySelector('#home-container');
+const quizContainer = document.querySelector('#quiz-container');
+const userFinalScoreContainer = document.querySelector('#user-final-score-container');
+const highScoresContainer = document.querySelector('#high-score-container');
