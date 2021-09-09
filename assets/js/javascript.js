a28900c2f52abdf356f1c87baef1847211f97e1d
David Kitley-McNamara <david@kitley-mcnamara.com>
2021-09-09 14:51:35 +0100

    feat: Added an offline message - Proberbly not needed as the user has opened the page, but it covers any fetch api errors

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -5,7 +5,6 @@ const userFinalScoreContainer = document.querySelector("#user-final-score-contai
 const highScoresContainer = document.querySelector("#high-score-container");
 const exitQuizContainer = document.querySelector("#exit-quiz-container");
 
-
 //* grab the button elements for the event listeners
 const playButton = document.querySelector("#btn-play-game");
 const homeScreenButton = document.querySelector("#btn-view-home-screen");
@@ -23,7 +22,7 @@ const pointsPerCorrectAnswerEasy = 1; //* points for easy questions
 const pointsPerCorrectAnswerMedium = 1.5; //* points for medium questions
 const pointsPerCorrectAnswerHard = 2; //* points for hard questions
 let pointsPerCorrectAnswer = pointsPerCorrectAnswerEasy; //default value for easy - 
-const SetQtyOfQuestions = 10; //* amount of questions for the quiz
+const SetQtyOfQuestions = 3; //* amount of questions for the quiz
 const highScoresToShow = 8; //* amount of high scores to shw in high score list
 const qtyOfQuestionsToFetch = (SetQtyOfQuestions * 5); //* increase this value to increase the randomness of the questions, only fetching SetQtyOfQuestions value only pulls from the first section of the API 
 
@@ -42,8 +41,8 @@ const username = document.getElementById("username");
 let highScores = [];
 const highScoresList = document.querySelector(".highScoresList");
 const endGameHighScoresList = document.querySelector(".endGameHighScoresList");
-
 const loadingSpinner = document.querySelector(".loadingSpinner");
+const saveHighScore = document.querySelector("#btn-save-score");
 
 let getNewQuestion;
 let incrementScore;
@@ -73,36 +72,47 @@ unMuteButton.addEventListener("click", sounds);
 showExitGameOptions.addEventListener("click", showExitQuizContainer);
 exitGame.addEventListener("click", returnToHomeScreen);
 continuePlayingButton.addEventListener("click", closeExitOverlayScreen);
-
 playButton.addEventListener("click", startQuiz);
+saveHighScore.addEventListener("click", saveTheHighScore);
 
 //* This function removes the items from the session storage on refresh
-window.location.reload = function () {
+window.location.reload = function ()
+{
 	sessionStorage.removeItem('hasSampleScoresBeenAddedBefore');
 	sessionStorage.removeItem('highScores');
 	sessionStorage.removeItem('sounds');
 };
 
 //* gets and add the session storage altering the key(sounds) from mute to play
-function sounds() {
-	if (sessionStorage.getItem("sounds") == undefined) {
+function sounds()
+{
+	if (sessionStorage.getItem("sounds") == undefined)
+	{
 		sessionStorage.setItem("sounds", "mute");
-	} else if (sessionStorage.getItem("sounds") == "mute") {
+	}
+	else if (sessionStorage.getItem("sounds") == "mute")
+	{
 		sessionStorage.setItem("sounds", "play");
-	} else {
+	}
+	else
+	{
 		sessionStorage.setItem("sounds", "mute");
 	}
 	sfxMuteOrPlay();
 }
 
 //* alternates the SFX button and mutes/plays the SFX accordingly
-function sfxMuteOrPlay() {
-	if (sessionStorage.getItem("sounds") == "mute") {
+function sfxMuteOrPlay()
+{
+	if (sessionStorage.getItem("sounds") == "mute")
+	{
 		soundCorrect.muted = true;
 		soundIncorrect.muted = true;
 		muteButton.classList.add("hidden");
 		unMuteButton.classList.remove("hidden");
-	} else {
+	}
+	else
+	{
 		soundCorrect.muted = false;
 		soundIncorrect.muted = false;
 		unMuteButton.classList.add("hidden");
@@ -110,13 +120,16 @@ function sfxMuteOrPlay() {
 	}
 }
 
+
 /** Function to add the points information to the home screen. */
-function addPointsInformationToTheHomePage() {
+function addPointsInformationToTheHomePage()
+{
 	let pointsInformation = document.getElementById("points-information");
 	let pointsInformationText = `Easy - ${pointsPerCorrectAnswerEasy} point per question, Medium - ${pointsPerCorrectAnswerMedium} points per question<br> and Hard - ${pointsPerCorrectAnswerHard} points per question`;
 	pointsInformation.innerHTML = pointsInformationText;
 }
 addPointsInformationToTheHomePage();
+
 
 //* Quiz difficulty and API information
 let level = document.getElementById("selectLevelRef").value;
@@ -124,7 +137,8 @@ let quizUrl = `https://opentdb.com/api.php?amount=${qtyOfQuestionsToFetch}&categ
 
 
 //* function to hide the welcome page and show the quiz
-function showQuizContainer() {
+function showQuizContainer()
+{
 	homeContainer.classList.add("hidden");
 	quizContainer.classList.remove("hidden");
 	muteButton.classList.remove("hidden");
@@ -132,7 +146,8 @@ function showQuizContainer() {
 }
 
 //* function to return to the home screen
-function returnToHomeScreen() {
+function returnToHomeScreen()
+{
 	//* just in case some containers are visible this will add the hidden class to them
 	quizContainer.classList.add("hidden");
 	userFinalScoreContainer.classList.add("hidden");
@@ -147,46 +162,50 @@ function returnToHomeScreen() {
 }
 
 //* function to hide the welcome page and show the quiz
-function showExitQuizContainer() {
+function showExitQuizContainer()
+{
 	exitQuizContainer.classList.remove("hidden");
 
 }
 
 //* function Exit quiz Yes or No
-function closeExitOverlayScreen() {
+function closeExitOverlayScreen()
+{
 	exitQuizContainer.classList.add("hidden");
 }
 
 
 /** function to add sample points to the session storage, The first statement checks if the one time function has NOT been executed before */
-window.onload = function () {
-	if (sessionStorage.getItem("hasSampleScoresBeenAddedBefore") == null) {
+window.onload = function ()
+{
+	if (sessionStorage.getItem("hasSampleScoresBeenAddedBefore") == null)
+	{
 		/** this is to add some sample high scores to local storage */
-		let letsAddSomeSampleHighScores = [{
-				"score": Math.floor(Math.random() * (SetQtyOfQuestions + 1)) * pointsPerCorrectAnswerHard,
-				"name": "Ms PacMan"
-			},
-			{
-				"score": Math.floor(Math.random() * (SetQtyOfQuestions + 1)) * pointsPerCorrectAnswerHard,
-				"name": "Gandalf"
-			},
-			{
-				"score": Math.floor(Math.random() * (SetQtyOfQuestions + 1)) * pointsPerCorrectAnswerMedium,
-				"name": "ALF"
-			},
-			{
-				"score": Math.floor(Math.random() * (SetQtyOfQuestions + 1)) * pointsPerCorrectAnswerMedium,
-				"name": "Kermit"
-			},
-			{
-				"score": Math.floor(Math.random() * (SetQtyOfQuestions + 1)) * pointsPerCorrectAnswerEasy,
-				"name": "Miss Piggy"
-			},
-			{
-				"score": Math.floor(Math.random() * (SetQtyOfQuestions + 1)) * pointsPerCorrectAnswerEasy,
-				"name": "Papa Smurf"
-			}
-		];
+		let letsAddSomeSampleHighScores = [
+		{
+			"score": Math.floor(Math.random() * (SetQtyOfQuestions + 1)) * pointsPerCorrectAnswerHard,
+			"name": "Ms PacMan"
+		},
+		{
+			"score": Math.floor(Math.random() * (SetQtyOfQuestions + 1)) * pointsPerCorrectAnswerHard,
+			"name": "Gandalf"
+		},
+		{
+			"score": Math.floor(Math.random() * (SetQtyOfQuestions + 1)) * pointsPerCorrectAnswerMedium,
+			"name": "ALF"
+		},
+		{
+			"score": Math.floor(Math.random() * (SetQtyOfQuestions + 1)) * pointsPerCorrectAnswerMedium,
+			"name": "Kermit"
+		},
+		{
+			"score": Math.floor(Math.random() * (SetQtyOfQuestions + 1)) * pointsPerCorrectAnswerEasy,
+			"name": "Miss Piggy"
+		},
+		{
+			"score": Math.floor(Math.random() * (SetQtyOfQuestions + 1)) * pointsPerCorrectAnswerEasy,
+			"name": "Papa Smurf"
+		}];
 
 		sessionStorage.setItem("highScores", JSON.stringify(letsAddSomeSampleHighScores));
 		sessionStorage.setItem("hasSampleScoresBeenAddedBefore", true);
@@ -198,7 +217,8 @@ window.onload = function () {
 
 
 //* function to start the game and any previous scores 
-function startQuiz() {
+function startQuiz()
+{
 	scoreText.innerText = 0;
 	score = 0;
 	playerFinalScore.innerText = `You scored ${score}`;
@@ -214,63 +234,47 @@ function startQuiz() {
 
 /** function to set the correct points per question dependant on the user selected difficulty level
  */
-function pointsPerQuestion() {
-	if (level == "hard") {
+function pointsPerQuestion()
+{
+	if (level == "hard")
+	{
 		pointsPerCorrectAnswer = pointsPerCorrectAnswerHard;
-	} else if (level == "medium") {
+	}
+	else if (level == "medium")
+	{
 		pointsPerCorrectAnswer = pointsPerCorrectAnswerMedium;
-	} else {
+	}
+	else
+	{
 		pointsPerCorrectAnswer = pointsPerCorrectAnswerEasy;
 	}
 }
 
 /** function to sort the high scores numerically */
-function highScoresRetrieveAndSort() {
+function highScoresRetrieveAndSort()
+{
 	highScores = JSON.parse(sessionStorage.getItem("highScores")) || [];
-	highScores.sort((a, b) => {
+	highScores.sort((a, b) =>
+	{
 		return b.score - a.score;
 	});
 }
 
 /** function to allow the user to select a difficulty level for the quiz */
-function updateQuizLevel() {
+function updateQuizLevel()
+{
 	level = document.getElementById("selectLevelRef").value;
 	quizUrl = `https://opentdb.com/api.php?amount=${qtyOfQuestionsToFetch}&category=9&difficulty=${level}&type=multiple`;
 	pointsPerQuestion();
 	sessionStorage.setItem("API-URL", quizUrl);
 }
 
-/** Function to fetch the questions from an API using the user selected difficulty as the quiz level and map the question to an array */
-fetch(quizUrl)
-	.then((res) => {
-		return res.json();
-	})
-	.then((loadedQuestions) => {
-		questions = loadedQuestions.results.map((loadedQuestion) => {
-			const formattedQuestion = {
-				question: loadedQuestion.question,
-			};
-			const availableAnswers = [...loadedQuestion.incorrect_answers];
-			formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
-			availableAnswers.splice(
-				formattedQuestion.answer - 1,
-				0,
-				loadedQuestion.correct_answer
-			);
-
-			availableAnswers.forEach((answers, index) => {
-				formattedQuestion["answers" + (index + 1)] = answers;
-			});
-
-			return formattedQuestion;
-		});
-
-	});
-
 /**Function checks if the maximum amount of questions per quiz has been reached */
-function maxQuestionsReached() {
+function maxQuestionsReached()
+{
 	// checking if maximum availableQuestions has been reached and if so go to user final score page
-	if (availableQuestions.length === 0 || questionCounter >= SetQtyOfQuestions) {
+	if (availableQuestions.length === 0 || questionCounter >= SetQtyOfQuestions)
+	{
 		sessionStorage.setItem("mostRecentScore", score);
 		endGameHighScores();
 		quizContainer.classList.add("hidden");
@@ -280,82 +284,9 @@ function maxQuestionsReached() {
 	}
 }
 
-/**  Function to sort the questions */
-getNewQuestion = () => {
-	maxQuestionsReached();
-
-	questionCounter++;
-
-	//Updates the progress bar
-	progressText.innerText = `Question ${questionCounter}/${SetQtyOfQuestions}`;
-
-	progressBarFull.style.width = `${(questionCounter / SetQtyOfQuestions) * 100}%`;
-	if (availableQuestions.length === 0 || questionCounter >= SetQtyOfQuestions) {
-		progressBarFull.classList.add("progress-bar-rounded");
-	}
-
-	//creates a random number between 1 and the qty of remaining questions and sets the current question to that question number
-	const questionIndex = Math.floor(Math.random() * (qtyOfQuestionsToFetch - (questionCounter - 1)));
-	currentQuestion = availableQuestions[questionIndex];
-
-	// adds current question to the Question section 
-	question.innerHTML = currentQuestion.question;
-
-	// gets the correct answer information from the set of questions
-	answers.forEach((answers) => {
-		const number = answers.dataset.number;
-		answers.innerHTML = currentQuestion["answers" + number];
-	});
-	//removes the current question from the available questions list
-	availableQuestions.splice(questionIndex, 1);
-	acceptingAnswers = true;
-};
-
-//*check which answer the user has chosen
-answers.forEach((answers) => {
-	answers.addEventListener("click", (e) => {
-		if (!acceptingAnswers) return;
-
-		acceptingAnswers = false;
-		const selectedAnswers = e.target;
-		const selectedAnswer = selectedAnswers.dataset.number;
-
-		//check if the user has selected the correct answer 
-		const classToApply = selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
-		//if  answer correct increase the user score
-		if (classToApply === "correct") {
-			incrementScore(pointsPerCorrectAnswer);
-			soundCorrect.play();
-		} else {
-			soundIncorrect.play();
-		}
-
-		selectedAnswers.parentElement.classList.add(classToApply);
-
-		setTimeout(() => {
-			selectedAnswers.parentElement.classList.remove(classToApply);
-			getNewQuestion();
-		}, 1500);
-	});
-});
-
-
-//** Function to update the users score */ 
-incrementScore = (questionPointsValue) => {
-	score += questionPointsValue;
-	scoreText.innerText = score;
-	playerFinalScore.innerText = `You scored ${score}`;
-};
-
-//*event listener to allow user to click the save button once username entered
-username.addEventListener("keyup", () => {
-	saveScoreBtn.disabled = !username.value;
-});
-
-const saveHighScore = document.querySelector("#btn-save-score");
-saveHighScore.addEventListener("click", saveTheHighScore);
 //*function to save the high score
-function saveTheHighScore(submit) {
+function saveTheHighScore(submit)
+{
 
 	submit.preventDefault();
 
@@ -373,10 +304,12 @@ function saveTheHighScore(submit) {
 }
 
 //*high scores added to the high score list if user saves the score
-function endGameHighScores() {
+function endGameHighScores()
+{
 	highScoresRetrieveAndSort();
 	endGameHighScoresList.innerHTML = highScores
-		.map(score => {
+		.map(score =>
+		{
 			return `<li class="high-score"><span>${score.score}</span><span>${score.name}</span</li>`;
 		})
 		.join("");
@@ -384,15 +317,145 @@ function endGameHighScores() {
 
 
 //* function to show the high Scores
-function showHighScoresScreen() {
+function showHighScoresScreen()
+{
 	highScoresRetrieveAndSort();
 
 	homeContainer.classList.add("hidden");
 	highScoresContainer.classList.remove("hidden");
 
 	highScoresList.innerHTML = highScores
-		.map(score => {
+		.map(score =>
+		{
 			return `<li class="high-score"><span>${score.score}</span><span>${score.name}</span</li>`;
 		})
 		.join("");
+}
+
+//* check if connected to the internet
+let ifConnected = window.navigator.onLine;
+const noInternetAlert = document.getElementById("no-internet");
+if (ifConnected)
+{
+	/** Function to fetch the questions from an API using the user selected difficulty as the quiz level and map the question to an array */
+
+	fetch(quizUrl)
+		.then((res) =>
+		{
+			return res.json();
+		})
+		.then((loadedQuestions) =>
+		{
+			questions = loadedQuestions.results.map((loadedQuestion) =>
+			{
+				const formattedQuestion = {
+					question: loadedQuestion.question,
+				};
+				const availableAnswers = [...loadedQuestion.incorrect_answers];
+				formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
+				availableAnswers.splice(
+					formattedQuestion.answer - 1,
+					0,
+					loadedQuestion.correct_answer
+				);
+
+				availableAnswers.forEach((answers, index) =>
+				{
+					formattedQuestion["answers" + (index + 1)] = answers;
+				});
+
+				return formattedQuestion;
+			});
+
+		});
+
+
+	/**  Function to sort the questions */
+	getNewQuestion = () =>
+	{
+		maxQuestionsReached();
+
+		questionCounter++;
+
+		//Updates the progress bar
+		progressText.innerText = `Question ${questionCounter}/${SetQtyOfQuestions}`;
+
+		progressBarFull.style.width = `${(questionCounter / SetQtyOfQuestions) * 100}%`;
+		if (availableQuestions.length === 0 || questionCounter >= SetQtyOfQuestions)
+		{
+			progressBarFull.classList.add("progress-bar-rounded");
+		}
+
+		//creates a random number between 1 and the qty of remaining questions and sets the current question to that question number
+		const questionIndex = Math.floor(Math.random() * (qtyOfQuestionsToFetch - (questionCounter - 1)));
+		currentQuestion = availableQuestions[questionIndex];
+
+		// adds current question to the Question section 
+		question.innerHTML = currentQuestion.question;
+
+		// gets the correct answer information from the set of questions
+		answers.forEach((answers) =>
+		{
+			const number = answers.dataset.number;
+			answers.innerHTML = currentQuestion["answers" + number];
+		});
+		//removes the current question from the available questions list
+		availableQuestions.splice(questionIndex, 1);
+		acceptingAnswers = true;
+	};
+
+	//*check which answer the user has chosen
+	answers.forEach((answers) =>
+	{
+		answers.addEventListener("click", (e) =>
+		{
+			if (!acceptingAnswers) return;
+
+			acceptingAnswers = false;
+			const selectedAnswers = e.target;
+			const selectedAnswer = selectedAnswers.dataset.number;
+
+			//check if the user has selected the correct answer 
+			const classToApply = selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+			//if  answer correct increase the user score
+			if (classToApply === "correct")
+			{
+				incrementScore(pointsPerCorrectAnswer);
+				soundCorrect.play();
+			}
+			else
+			{
+				soundIncorrect.play();
+			}
+
+			selectedAnswers.parentElement.classList.add(classToApply);
+
+			setTimeout(() =>
+			{
+				selectedAnswers.parentElement.classList.remove(classToApply);
+				getNewQuestion();
+			}, 1500);
+		});
+	});
+
+
+	//** Function to update the users score */ 
+	incrementScore = (questionPointsValue) =>
+	{
+		score += questionPointsValue;
+		scoreText.innerText = score;
+		playerFinalScore.innerText = `You scored ${score}`;
+	};
+
+	//*event listener to allow user to click the save button once username entered
+	username.addEventListener("keyup", () =>
+	{
+		saveScoreBtn.disabled = !username.value;
+	});
+
+}
+else
+{
+	noInternetAlert.classList.remove("hidden");
+
 }
